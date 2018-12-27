cd2da4d1f1f4dc8dcc1837c330f4a173203c9387
Sellari <tre@datadog.com>
2018-12-26 19:40:05 -0600

    Changed to Headless testing

--- SellariSelenium/src/test/java/SellariSelenium/SellariSelenium/AppTest.java
+++ SellariSelenium/src/test/java/SellariSelenium/SellariSelenium/AppTest.java
@@ -7,6 +7,8 @@ import junit.framework.TestSuite;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
 import io.github.bonigarcia.wdm.ChromeDriverManager;
 
 
@@ -40,33 +42,44 @@ public class AppTest
     public void testApp()
     {
     	ChromeDriverManager.getInstance().setup();
-        WebDriver driver = new ChromeDriver();
-        driver.manage().window().maximize();
+    	ChromeOptions options = new ChromeOptions();
+        options.addArguments("headless");
+        options.addArguments("window-size=1200x600");    	
+        WebDriver driver = new ChromeDriver(options);
+        //driver.manage().window().maximize();
         
+        System.out.println("Navigating to Dallas Stars Website");
         driver.get("https://www.nhl.com/stars");
         
+        System.out.println("Opening Stats");
         driver.findElement(By.xpath("//*[@id=\"team-landing_index\"]/div[2]/div[3]/div[2]/div/nav[2]/ul/li[5]/a")).click();
         driver.findElement(By.xpath("//*[@id=\"team-landing_index\"]/div[2]/div[3]/div[2]/div/nav[2]/ul/li[5]/a")).click();
         
+        System.out.println("Selecting Tyler Seguin");
         driver.get("https://www.nhl.com/player/tyler-seguin-8475794");
        
         try
         {
 
+        	System.out.println("Capturing Name and Number");
         	String info = driver.findElement(By.xpath("//*[@id=\"content-wrap\"]/section[1]/div/section/h3")).getText();
         	
+        	System.out.println("Checking if the name and number are correct");
         	if(info.contains("Seguin") && info.contains("#92"))
 			{
+        		System.out.println("Correct!");
 				assertTrue(true);
 			}
 			else
 			{
+				System.out.println("Not Correct");
 				assertTrue(false);
 			}
         	
         }
         catch(Exception e)
         {
+        	System.out.println("Error Occurred");
         	assertTrue(false);
         }
        
